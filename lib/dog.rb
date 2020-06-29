29b52af85b906aaa51b987db438e376ed4ca5616
Hal H Nader <halnader89@gmail.com>
2020-06-29 22:30:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,5 +7,20 @@ class Dog
     save
   end
   
+  def self.all
+        return @@all
+  end
+
+  def self.clear_all
+        @@all.clear
+  end
+    
+  def self.print_all
+        @@all.map { |dog| puts dog.name }
+  end
+
+  def save
+        @@all << self
+  end
 
 end
